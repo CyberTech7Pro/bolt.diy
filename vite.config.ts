f3f0916527e8aad342a05a5e1e2b0c82010525d8
CyberTech7Pro <aicybertechweb@gmail.com>
2025-08-14 18:35:42 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vitePlugin as remix } from '@remix-run/dev'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import UnoCSS from 'unocss/vite'
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [
-    react(),
+    remix(),          // ✅ plugin obrigatório do Remix
     tsconfigPaths(),
     UnoCSS()
   ],
@@ -24,4 +24,4 @@ export default defineConfig(() => ({
     host: true,
     port: 5173
   }
-}))
+})
